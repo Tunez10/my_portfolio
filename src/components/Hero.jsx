d6266b801328af4da1d12a24fe92e4cabe98264d
Tunez10 <peterekene162@gmail.com>
2025-06-02 17:36:24 +0100

    New features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {useTypewriter,Cursor } from 'react-simple-typewriter'
+import {useTypewriter, Cursor } from 'react-simple-typewriter'
 
 
 export default function Hero() {
@@ -14,7 +14,7 @@ const [text] = useTypewriter({
     <div id='hero'>
         <div className="absolute top-1/2 left-1/2 text-center -translate-x-1/2 -translate-y-1/2 w-full">
           <p className="md:text-6xl font-bold text-white text-3xl">I am Fortune Peter</p>
-          <p className="font-bold text-teal-600 md:text-6xl text-3xl">{text}</p>
+          <p className="font-bold text-teal-600 md:text-6xl text-3xl">{text} <Cursor cursorColor='teal-600' /></p>
         </div>
     </div>
   )
